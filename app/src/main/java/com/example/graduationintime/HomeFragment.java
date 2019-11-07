bc07047406aa7eb9c94f7a3114938de84d003457
massicalic <50745950+massicalic@users.noreply.github.com>
2019-11-07 14:05:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/graduationintime/HomeFragment.java
@@ -0,0 +1,41 @@
+package com.example.graduationintime;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    private AppCompatActivity activity;
+    private View view;
+    private Toolbar toolbar;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        activity.setSupportActionBar(toolbar);
+        return view;
+    }
+
+    public void setActivity(AppCompatActivity activity) {
+        this.activity = activity;
+    }
+}
---
+++ app/src/main/java/com/example/graduationintime/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.graduationintime;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private HomeFragment home = new HomeFragment();
+    private DiaryFragment diary = new DiaryFragment();
+    private ProfileFragment profile = new ProfileFragment();
+    private AppCompatActivity activity;
+    private BottomNavigationView bottomNavigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int selectedItemId;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    selectedItemId = bottomNavigationView.getSelectedItemId();
+                    if (selectedItemId != R.id.navigation_home) {
+                        home.setActivity(activity);
+                        addFragment(home);
+                    }
+                    return true;
+                case R.id.navigation_diary:selectedItemId = bottomNavigationView.getSelectedItemId();
+                    if (selectedItemId != R.id.navigation_diary) {
+                        diary.setActivity(activity);
+                        addFragment(diary);
+                    }
+                    return true;
+
+                case R.id.navigation_profile:
+                    selectedItemId = bottomNavigationView.getSelectedItemId();
+                    if (selectedItemId != R.id.navigation_profile) {
+                        profile.setActivity(activity);
+                        addFragment(profile);
+                    }
+                return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        activity = this;
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        home.setActivity(activity);
+        addFragment(home);
+    }
+
+    private void addFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_ancor, fragment);
+        fragmentTransaction.commit();
+    }
+}
